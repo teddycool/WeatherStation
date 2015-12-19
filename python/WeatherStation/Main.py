3a169c8d81a191609fea3ee55e30ced7f34a1703
teddycool <par@sundback.com>
2015-12-19 11:18:22 +0100

    Removed screen and only sending values to db

--- python/WeatherStation/Main.py
+++ python/WeatherStation/Main.py
@@ -9,56 +9,24 @@ from WeatherStationConfig import config
 class Main(object):
 
     def __init__(self):
-        # Init framebuffer/touchscreen environment variables for raspberry
-       # if  os.sys.platform != 'win32':
-           # GPIO.setmode(GPIO.BCM)
-           # GPIO.setup(18,GPIO.OUT)
-            #GPIO.output(18, False)
-            #self._lightcontrolPwm =  GPIO.PWM(18, 50) #Pin 18 for backlight control and using default 50 hz
-            #self._lightcontrolPwm.ChangeDutyCycle(20)
-        print "Starting touchscreen"
-        os.putenv('SDL_VIDEODRIVER', 'fbcon')
-        os.putenv('SDL_FBDEV'      , '/dev/fb1')
-        os.putenv('SDL_MOUSEDRV'   , 'TSLIB')
-        os.putenv('SDL_MOUSEDEV'   , '/dev/input/touchscreen')
-        print "Init pygame..."
-        pygame.init()
-        pygame.mouse.set_visible(False)
+
         print "Init Main object..."
         self._mainLoop=MainLoop.MainLoop()
-        print "Setup screen"
-        self.screen = pygame.display.set_mode((320,240))
-        self._lastScreenUpdate = 0
-
 
     def run(self):
         print "Start run-loop..."
-
-
         self._mainLoop.initialize()
-        stopped = False
-        try:
-            while not stopped:
-                pos = (0,0)
-                #TODO: add  keyboard catch for abort, ctrl-c
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        sys.exit()
-                    if(event.type is MOUSEBUTTONDOWN):
-                        pos = pygame.mouse.get_pos()
-                #TODO: handle backlight and screen timeout
-                self._mainLoop.update(self.screen, pos)
-                self._mainLoop.draw(self.screen)
-                pygame.display.flip()
-                #TODO: set framerate based on time for each frame
-                while time.time() - self._lastScreenUpdate < config["UpdateInterval"]["Screen"]:
-                    time.sleep(0.01)
-                self._lastScreenUpdate = time.time()
-        finally:
-            return
-            #self._lightcontrolPwm.ChangeDutyCycle(100)
-            #self._lightcontrolPwm.stop()
-            #GPIO.cleanup()
+        running = True
+        while running:
+            try:
+                self._mainLoop.update()
+                time.sleep(1)
+            except Exception as e:
+                running = False
+                print str(e)
+                #self._lightcontrolPwm.ChangeDutyCycle(100)
+                #self._lightcontrolPwm.stop()
+                #GPIO.cleanup()
 
 #Testcode to run module. Standard Python way of testing modules.
 
--- python/WeatherStation/MainLoop.py
+++ python/WeatherStation/MainLoop.py
@@ -3,31 +3,17 @@ __author__ = 'teddycool'
 import pygame
 import time
 import sys
-from StateLoops import CurrentValues
 from Sensors import Sensors
-from StateLoops import MaxMin
-from StateLoops import OutdoorTrend
-from StateLoops import FridgeFreezerTrend
-from StateLoops import SaverScreen
 from WeatherStationConfig import config
 from Server import Server
 
 
 class MainLoop(object):
     def __init__(self):
-        #TODO: fix dynamic backlight depending on screentype and (later) lightsensor
-        #self._lightcontrolPwm = pwm
-        #self._lightcontrolPwm.ChangeDutyCycle(10) #Setting backlight to 20%
-        #print "setting pwm dutycycle to 10%"
+
         self._sensors=Sensors.Sensors()
-        self._server= Server.Server()
-        self._currentState = CurrentValues.CurrentValues()
-        self._maxMin = MaxMin.MaxMin()
-        self._outdoorTrend = OutdoorTrend.OutdoorTrend()
-        self._fridgeFreezerTrend = FridgeFreezerTrend.FridgeFreezerTrend()
-        self._saverScreen = SaverScreen.SaverScreen()
-        self._state = {"CurrentValues": self._currentState, "MaxMin": self._maxMin, "OutdoorTrend":self._outdoorTrend, "FridgeFreezerTrend":self._fridgeFreezerTrend, "SaverScreen":self._saverScreen}
-        self._currentState = self._state["SaverScreen"]
+        self._server = Server.Server()
+
         self._lastSensorUpdate = 0
         self._lastServerPush = 0
         return
@@ -35,14 +21,11 @@ class MainLoop(object):
     def initialize(self):
         print "Main init..."
         self.time=time.time()
-        #Init all states
-        for key in self._state.keys():
-            self._state[key].initialize(self._sensors)
         self._sensors.initialize()
         print "Station started at ", self.time
 
-    def update(self,screen, pos):
-        #TODO: Add range check
+    def update(self):
+        print "MainLoop update..."
         if time.time() - self._lastSensorUpdate > config["UpdateInterval"]["Sensors"]:
             self._sensors.update()
             self._lastSensorUpdate = time.time()
@@ -50,23 +33,3 @@ class MainLoop(object):
             self._server.push(self._sensors)
             self._lastServerPush = time.time()
 
-        newstate = self._currentState.update(pos)
-        if newstate != None:
-            self._changeState(newstate)
-
-    def draw(self, screen):
-        black=0,0,0
-        screen.fill(black)
-        self._currentState.draw(screen, self._sensors)
-
-
-        return screen
-
-    def _changeState(self, newstate):
-        if newstate not in self._state:
-            self._currentState = CurrentValues.CurrentValues()
-        else:
-            if self._currentState != self._state[newstate]:
-                self._currentState = self._state[newstate]
-                self._currentState.initialize(self._sensors)
-        return newstate
